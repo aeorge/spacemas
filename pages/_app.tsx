c44f687700a4d31b7fe1286c4979926926c03902
Georgios Kotziabassis <kotziabassis@gmail.com>
2021-12-12 22:15:37 +0100

    ✨ Add snowfall animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Snowfall from 'react-snowfall'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Snowfall style={{ zIndex: 9999 }} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
